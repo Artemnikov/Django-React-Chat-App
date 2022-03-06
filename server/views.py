e7dfb9472eb70a4b389c626d6e1a20d5498a3d2b
Artemnikov <sartem.meshkov@gmail.com>
2022-03-06 09:50:01 +0200

    added azure sso

--- server/views.py
+++ server/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from .models import Room, Message
 from django.views.decorators.csrf import csrf_exempt
 import json
+
+from django.urls import reverse
+from chat_app.auth_helper import get_sign_in_flow, get_token_from_code, store_user, remove_user_and_token, get_token
+from chat_app.graph_helper import *
 
 # Create your views here.
 @csrf_exempt
@@ -48,3 +52,39 @@ def send(request):
     new_message = Message.objects.create(value=message, user=user, room=room)
     new_message.save()
     return HttpResponse(new_message)
+
+
+def initialize_context(request):
+    context = {}
+    error = request.session.pop('flash_error', None)
+    if error != None:
+      context['errors'] = []
+    context['errors'].append(error)
+    # Check for user in the session
+    context['user'] = request.session.get('user' , {'is_authenticated': False})
+    return context
+
+def sign_in(request):
+    # Get the sign-in flow
+    flow = get_sign_in_flow()
+    # Save the expected flow so we can use it in the callback
+    try:
+        request.session['auth_flow'] = flow
+    except Exception as e:
+        print(e)
+    # Redirect to the Azure sign-in page
+    return HttpResponseRedirect(flow['auth_uri'])
+
+def sign_out(request):
+    # Clear out the user and token
+    remove_user_and_token(request)
+    return HttpResponseRedirect(reverse('home'))
+
+def callback(request):
+    # Make the token request
+    result = get_token_from_code(request)
+    #Get the user's profile from graph_helper.py script
+    user = get_user(result['access_token']) 
+    # Store user from auth_helper.py script
+    store_user(request, user)
+    return HttpResponseRedirect(reverse('home'))
