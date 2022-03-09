879bf1e11dd47bb1d878d52f316833921f783fde
Artemnikov <sartem.meshkov@gmail.com>
2022-03-09 14:34:33 +0200

    changes: made jwtToken

--- server/views.py
+++ server/views.py
@@ -4,58 +4,44 @@ from .models import Room, Message
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.views.decorators.cache import never_cache
-from chat_app.auth_helper import get_sign_in_flow, get_token_from_code, store_user, remove_user_and_token, get_token
-from chat_app.graph_helper import *
+from chat_app.auth_helper import get_sign_in_flow, get_token_from_code, remove_user_and_token, get_token
+from rest_framework_jwt.views import obtain_jwt_token
 
-# Create your views here.
+
 @csrf_exempt
-def main (request):
-    room =  json.loads(request.body).get('room')
-    username = json.loads(request.body).get('username')
-    room_details = Room.objects.get(name=room)
-    
-    return HttpResponse({
-        'username': username,
-        'room': room,
-        'room_details': room_details
-    })
-    
-@csrf_exempt
-def checkroom(request):
+def checkroom ( request ):
     room = json.loads(request.body).get('room')
     username = json.loads(request.body).get('username')
     data = json.dumps({
         'room': room,
         'username': username
     })
+    
     if Room.objects.filter(name=room).exists():
-        return HttpResponse(data)
+        return HttpResponse('room found')
     else:
         new_room = Room.objects.create(name=room)
         new_room.save()
-        return HttpResponse(data)
+        return HttpResponse('room created')
 
 @csrf_exempt
-def getMessages(request):
+def getMessages ( request ):
     room_req = json.loads(request.body).get('room')
     room_detail = Room.objects.get(name=room_req)
     messages = Message.objects.filter(room=room_detail.name)
-    print(messages)
     return JsonResponse({"messages" : list(messages.values())})
-    
 
 @csrf_exempt
-def send(request):
-    message = json.loads(request.body).get('message')
-    user = json.loads(request.body).get('username')
-    room = json.loads(request.body).get('room')
-
-    new_message = Message.objects.create(value=message, user=user, room=room)
+def send ( request ):
+    new_message = Message.objects.create(
+        value = json.loads(request.body).get('message'),
+        user = json.loads(request.body).get('username'),
+        room = json.loads(request.body).get('room')
+        )
     new_message.save()
-    return HttpResponse(new_message)
+    return HttpResponse('message saved')
 
-
-def initialize_context(request):
+def initialize_context ( request ):
     context = {}
     error = request.session.pop('flash_error', None)
     if error != None:
@@ -65,7 +51,7 @@ def initialize_context(request):
     context['user'] = request.session.get('user' , {'is_authenticated': False})
     return context
 
-def sign_in(request):
+def sign_in ( request ):
     # Get the sign-in flow
     flow = get_sign_in_flow()
     # Save the expected flow so we can use it in the callback
@@ -76,22 +62,19 @@ def sign_in(request):
     # Redirect to the Azure sign-in page
     return HttpResponseRedirect(flow['auth_uri'])
 
-def sign_out(request):
-    # Clear out the user and token
+def sign_out ( request ):
     remove_user_and_token(request)
-    return HttpResponseRedirect(reverse(''))
+    return HttpResponseRedirect('')
 
 
 @never_cache
-def callback(request):
+def callback ( request ):
     # Make the token request
     result = get_token_from_code(request)
     #Get the user's profile from graph_helper.py script]
     # user = get_user(result['access_token'])
     username = result['id_token_claims']['name']
-    username = username.replace(' ', '_')
-    return HttpResponseRedirect('/lobby/?name={}'.format(username))
-
-#  get jwt from azure sso
-# get public key from azure sso
-#  bearer token
+    response = HttpResponseRedirect('/lobby')
+    response.set_cookie('username', username)
+    response.set_cookie('wat', obtain_jwt_token("username=jwt&password=jwtadmin") )
+    return response
